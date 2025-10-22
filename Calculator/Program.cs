b5ca33639a61b58aaf67f7fdffad90eb75abd1aa
ryemaan <dhdjbzbsdv@gmail.com>
2025-10-22 10:33:06 +0500

    Update Program.cs

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,6 +1,8 @@
-﻿using Calculator;
+using Calculate;
+using Calculator;
 using System;
 Initialization initialization = new Initialization();
+SecondCalculating secondcalculating = new SecondCalculating();
 
 while (true)
 {
@@ -28,11 +30,52 @@ while (true)
     {
         Console.WriteLine(initialization.Division(num1, num2));
     }
-    Console.WriteLine("Хотите закончить программу?(да/нет)");
-    string Chose = Console.ReadLine();
-    if (Chose == "да") 
+    Console.WriteLine("Хотите ли вы завершить работу?");
+    string choose = Console.ReadLine();
+    if (choose == "да")
     {
-        break;
+        Console.WriteLine("Хотите ли вы попробовать другой калькулятор?");
+        string chose = Console.ReadLine();
+        if (choose == "да")
+        {
+            Console.WriteLine("Введите число:");
+            string Input = Console.ReadLine();
+            int num3 = int.Parse(Input);
+            Console.WriteLine("Выберите операцию(1 - возведение в квадрат, 2 - возведение в куб, 3 - корень числа, 4 - косинус, 5 - синус, 6 - тангенц, 7 - выход");
+            string Math = Console.ReadLine();
+            if (Math == "1")
+            {
+                Console.WriteLine(secondcalculating.Exponentiation2(num3));
+            }
+            else if (Math == "2")
+            {
+                Console.WriteLine(secondcalculating.Exponentiation3(num3));
+            }
+            else if (Math == "3")
+            {
+                Console.WriteLine(secondcalculating.Root(num3));
+            }
+            else if (Math == "4")
+            {
+                Console.WriteLine(secondcalculating.Cos(num3));
+            }
+            else if (Math == "5")
+            {
+                Console.WriteLine(secondcalculating.Sin(num3));
+            }
+            else if (Math == "6")
+            {
+                Console.WriteLine(secondcalculating.Tan(num3));
+            }
+            else if (Math == "7")
+            {
+                break;
+            }
+        }
+        else
+        {
+            break;
+        }
     }
     else
     {
