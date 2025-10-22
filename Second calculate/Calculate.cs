b4ee75ea7f3275b115bedf6c9af28beb18b1c9e7
ryemaan <dhdjbzbsdv@gmail.com>
2025-10-22 10:19:45 +0500

    Add files via upload

---
+++ Second calculate/Calculate.cs
@@ -0,0 +1,47 @@
+﻿using Second_calculate.ISecondCalculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Calculate
+{
+    internal class SecondCalculating : ISecondCalculator
+    {
+        const double pi = 3.14;
+        public int num3 { get; set; }
+        public int num4 { get; set; }
+
+        public int Exponentiation2(int num3)
+        {
+            return num3 * num3;
+        }
+        public int Exponentiation3(int num3)
+        {
+            return (num3 * num3 * num3);
+        }
+        public double Root(double num3)
+        {
+            double guess = num3 / 2;
+            for (int i = 0; i < 10; i++)
+            {
+                guess = (guess + num3 / guess) / 2;
+            }
+            return guess;
+        } 
+        public double Sin(int num3)
+        {
+            return (num3 * pi) / 60;
+        }
+        public double Cos(int num3)
+        {
+            return (num3 * pi) / 90;
+        }
+        public double Tan(int num3)
+        {
+            return (num3 * pi) / 180;
+        }
+    }
+}
---
+++ Second calculate/SecondCalculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Second_calculate.ISecondCalculator
+{   
+    internal interface ISecondCalculator
+    {
+        public int num3 { get; set; }
+        public int num4 { get; set; }
+        public int Exponentiation2(int num3);
+        public int Exponentiation3(int num3);
+        public double Root(double num3);
+        public double Sin(int num3);
+        public double Cos(int num3);
+        public double Tan(int num3);
+    }
+}
