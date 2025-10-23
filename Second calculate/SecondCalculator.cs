3633ac18e5dccc04a7dccf87f192b2004a7f50d4
ryemaan <dhdjbzbsdv@gmail.com>
2025-10-23 14:43:34 +0500

    Update SecondCalculator.cs

--- Second calculate/SecondCalculator.cs
+++ Second calculate/SecondCalculator.cs
@@ -1,11 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Second_calculate.ISecondCalculator
-{   
+{
     internal interface ISecondCalculator
     {
         public int num3 { get; set; }
@@ -13,8 +13,8 @@ namespace Second_calculate.ISecondCalculator
         public int Exponentiation2(int num3);
         public int Exponentiation3(int num3);
         public double Root(double num3);
-        public double Sin(int num3);
-        public double Cos(int num3);
-        public double Tan(int num3);
+        public double Sin(double num3);
+        public double Cos(double num3);
+        public double Tan(double num3);
     }
 }
